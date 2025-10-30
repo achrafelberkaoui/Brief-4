43fb4c6b68c1808267b6c1a2c409711f87543391
Your Name <achrafelberkoaui@gmail.com>
2025-10-30 23:27:33 +0100

    active button de reserve en js

--- java.js
+++ java.js
@@ -1,7 +1,12 @@
-let hello = document.getElementById("ev-choix")
+const events = document.querySelectorAll('.evenem-choix');
+const btnres = document.querySelectorAll('.res');
 
-function affichage() {
-    console.log("sdqg");
-    hello.classList.toggle("ev")
-
+for(let btn of btnres){
+    btn.addEventListener('click', function(){
+        for(let ev of events){
+            ev.classList.remove('selected');
+        }
+        const parenres = btn.parentElement.parentElement;
+        parenres.classList.add('selected');
+    })
 }
