f7296f315c80d3de2b0b0ac54a151650eec9130c
achrafelberkaoui <achrafelberkoaui@gmail.com>
2025-10-31 12:47:25 +0100

    probleme steps pages

--- java.js
+++ java.js
@@ -4,6 +4,10 @@ const nex = document.getElementById('next');
 const page1 = document.getElementById('page1');
 const page2 = document.getElementById('page2');
 const childNv = document.getElementById('schild-nv');
+const pre = document.getElementById('precedent');
+const steps = document.querySelectorAll('#step-v');
+const images = document.querySelectorAll('.step-va');
+const iimg = document.getElementById('cls-img');
 let selecdiv = null;
 for (let btn of btnres) {
 
@@ -21,13 +25,37 @@ for (let btn of btnres) {
 
 nex.addEventListener('click', function () {
     if (selecdiv) {
-        page1.style.display = 'none'; 
-        page2.style.display = 'block'; 
-        childNv.innerHTML = ''; 
-        childNv.appendChild(selecdiv); 
+        page1.style.display = 'none';
+        page2.style.display = 'block';
+        for(let i = 0; i<5; i++){
+        images[i].style.display = "block";
+
+        }
+        childNv.appendChild(selecdiv);
     } else {
         alert('veuillez Choisissez un eve !');
     }
+    if (!page1) {
+        pre.style.display = 'none';
+    }
+    else {
+        pre.style.display = 'block';
+    }
 })
+
+
+pre.addEventListener('click', function () {
+
+    page2.style.display = 'none';
+    page1.style.display = 'block';
+})
+
+
+
+
+
+
+
+
 
 
